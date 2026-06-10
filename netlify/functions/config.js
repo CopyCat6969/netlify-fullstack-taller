3a61ba2d5325284f462bcc07581ed6c87de15ee0
CopyCat6969 <nerazo1410@gmail.com>
2026-06-10 14:51:48 -0500

    Taller fullstack con Netlify

---
+++ netlify/functions/config.js
@@ -0,0 +1,10 @@
+exports.handler = async function() {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      appName: process.env.APP_NAME || "Sin nombre",
+      docente: process.env.DOCENTE || "Sin docente"
+    })
+  };
+};
